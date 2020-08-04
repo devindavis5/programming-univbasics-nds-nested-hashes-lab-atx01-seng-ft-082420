2b0d5280027cfdea83a8c760fb0b8acdc0f74265
Devin Davis <devinrdavis555@gmail.com>
2020-08-04 18:22:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,15 +24,16 @@ def literal_aoh
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
-  
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
+  
+  aoh[row][key] = new_value
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
